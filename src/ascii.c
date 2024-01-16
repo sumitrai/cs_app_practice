e2f27184ae65c2dbb149ce4a8b8e88e2b321c067
Sumit Rai <3071133+sumitrai@users.noreply.github.com>
2024-01-17 01:01:29 +0530

    Added ascii.c
    Unfinished program - just verifying if gpg key
    signing is workign.

---
+++ src/ascii.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "utils.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        PRINT_INFO("Usage: ascii /path/to/file/to/decode\n");
+    }
+    
+}
